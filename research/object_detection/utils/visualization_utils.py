675bd77d473e8a88911cae37ffee12a231ccde37
AaradhyaSaxena <aaradhya271197@gmail.com>
2020-07-25 14:49:35 +0530

    added extract_faces_from_image_array function

--- research/object_detection/utils/visualization_utils.py
+++ research/object_detection/utils/visualization_utils.py
@@ -70,6 +70,89 @@ STANDARD_COLORS = [
     'WhiteSmoke', 'Yellow', 'YellowGreen'
 ]
 
+def extract_faces_from_image_array(
+    image,boxes,classes,scores,category_index,instance_masks=None,
+    instance_boundaries=None,
+    keypoints=None,
+    keypoint_scores=None,
+    keypoint_edges=None,
+    track_ids=None,
+    use_normalized_coordinates=False,
+    max_boxes_to_draw=40,
+    min_score_thresh=.5,
+    agnostic_mode=False,
+    line_thickness=4,
+    groundtruth_box_visualization_color='black',
+    skip_boxes=True,
+    skip_scores=True,
+    skip_labels=True,
+    skip_track_ids=True):
+    
+    box_to_display_str_map = collections.defaultdict(list)
+    box_to_color_map = collections.defaultdict(str)
+    box_to_instance_masks_map = {}
+    box_to_instance_boundaries_map = {}
+    box_to_keypoints_map = collections.defaultdict(list)
+    box_to_keypoint_scores_map = collections.defaultdict(list)
+    box_to_track_ids_map = {}
+    ans = []
+    if not max_boxes_to_draw:
+        max_boxes_to_draw = boxes.shape[0]
+    for i in range(boxes.shape[0]):
+        if max_boxes_to_draw == len(box_to_color_map):
+            break
+        if scores is None or scores[i] > min_score_thresh:
+            box = tuple(boxes[i].tolist())
+            if instance_masks is not None:
+                box_to_instance_masks_map[box] = instance_masks[i]
+            if instance_boundaries is not None:
+                box_to_instance_boundaries_map[box] = instance_boundaries[i]
+            if keypoints is not None:
+                box_to_keypoints_map[box].extend(keypoints[i])
+            if keypoint_scores is not None:
+                box_to_keypoint_scores_map[box].extend(keypoint_scores[i])
+            if track_ids is not None:
+                box_to_track_ids_map[box] = track_ids[i]
+            if scores is None:
+                box_to_color_map[box] = groundtruth_box_visualization_color
+            else:
+                display_str = ''
+                if not skip_labels:
+                    if not agnostic_mode:
+                        if classes[i] in six.viewkeys(category_index):
+                            class_name = category_index[classes[i]]['name']
+                        else:
+                            class_name = 'N/A'
+                        display_str = str(class_name)
+                if not skip_scores:
+                    if not display_str:
+                        display_str = '{}%'.format(round(100*scores[i]))
+                    else:
+                        display_str = '{}: {}%'.format(display_str, round(100*scores[i]))
+                if not skip_track_ids and track_ids is not None:
+                    if not display_str:
+                        display_str = 'ID {}'.format(track_ids[i])
+                    else:
+                        display_str = '{}: ID {}'.format(display_str, track_ids[i])
+                box_to_display_str_map[box].append(display_str)
+                if agnostic_mode:
+                    box_to_color_map[box] = 'DarkOrange'
+                elif track_ids is not None:
+                    prime_multipler = _get_multiplier_for_color_randomness()
+                    box_to_color_map[box] = STANDARD_COLORS[(prime_multipler * track_ids[i]) % len(STANDARD_COLORS)]
+                else:
+                    box_to_color_map[box] = STANDARD_COLORS[classes[i] % len(STANDARD_COLORS)] 
+    count = 0
+    faces_mat = []
+    # Draw all boxes onto image.
+    for box, color in box_to_color_map.items():
+        count = count +1
+        faces_mat.append(box)
+    ## print(count)
+    
+    return faces_mat
+
+
 
 def _get_multiplier_for_color_randomness():
   """Returns a multiplier to get semi-random colors from successive indices.
